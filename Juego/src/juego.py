f5214470078e94dc2dcd31cfc1f45799dc5307ef
unlaso2016 <juanpbottega@gmail.com>
2016-08-19 17:21:37 -0300

    Add files via upload

---
+++ Juego/src/juego.py
@@ -0,0 +1,30 @@
+import pygame,sys
+from pygame.locals import *
+from random import randint
+
+pygame.init()
+ventana = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Prueba")
+
+imagen = pygame.image.load("Imagenes\ovni.png")
+posX = 270
+posY = 300
+
+velocidad = 5
+blanco = (255, 255, 255)
+derecha = True
+
+while True:
+    ventana.fill(blanco)
+    ventana.blit(imagen,(posX, posY))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_LEFT:
+                    posX -= velocidad
+            elif event.key == K_RIGHT:
+                    posX += velocidad
+                
+    pygame.display.update()
