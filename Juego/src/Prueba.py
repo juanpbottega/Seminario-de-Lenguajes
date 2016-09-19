01de64d557ab2952945c7b77e44d36919b89c240
juan patricio <juanpbottega@gmail.com>
2016-09-18 21:13:28 -0300

    agregada imagen para la curacion, disparo potenciado (laser) y multiples enemigos (usando listas)

--- Juego/src/Prueba.py
+++ Juego/src/Prueba.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from random import randint
 
 ancho = 1024
 alto = 768
@@ -18,46 +19,62 @@ class Personajes(pygame.sprite.Sprite):
                 self.velocidad = 5
                 self.listaDisparo = []
                 self.listaCuracion=[]
-                self.ultimoDisparo = 0
-                self.ultimaCuracion = 0
+                self.ultimoDisparo = -1
+                self.ultimaCuracion = -5
+                self.ultimoDisparoLaser=-5
                 self.direccion = 'N' #Direcciones: Norte(N), Sur(S), Este(E), Oeste(O)
                 self.Fuente = pygame.font.SysFont("Arial", 30)
                 self.cooldownCuracion=0
+                self.cooldownLaser=0
         
         def dibujar(self, superficie):
                 superficie.blit(self.imagenPersonaje, self.rect)
                 self.mostrarVida(superficie)
                 self.mostrarCoolCuracion(superficie)
+                self.mostrarCoolLaser(superficie)
                 
         def mostrarVida(self, superficie):
                 texto_vida = self.Fuente.render("Vida: " + str(self.vida), 0, (255, 0, 0))
                 superficie.blit(texto_vida, (5, 5))
                 
         def mostrarCoolCuracion(self,superficie):
-                texto_curacion = self.Fuente.render("Curacion: " + str(self.cooldownCuracion), 0, (255, 0, 0))
+                texto_curacion = self.Fuente.render("Curacion: " + str(self.cooldownCuracion), 0, (255, 255, 0))
                 superficie.blit(texto_curacion, (150, 5))
+        def mostrarCoolLaser(self,superficie):
+                texto_laser = self.Fuente.render("Laser: " + str(self.cooldownLaser), 0, (0, 255, 255))
+                superficie.blit(texto_laser, (300, 5))
                 
+        
+                
+        def mover(self, izq, der, arr, aba):
+                if izq:
+                        self.moverIzquierda()
+                elif der:
+                        self.moverDerecha()
+                elif arr:
+                        self.moverArriba()
+                elif aba:
+                        self.moverAbajo()
+           
         def moverIzquierda(self):
                 self.rect.centerx -= self.velocidad
                 self.__movimiento()
                 self.direccion = 'O'
-                #limite de pantalla()
 
         def moverDerecha(self):
                 self.rect.centerx += self.velocidad
                 self.__movimiento()
                 self.direccion = 'E'
-                #limite de pantalla()
+
         def moverArriba(self):
                 self.rect.centery -= self.velocidad
                 self.__movimiento()
                 self.direccion = 'N'
-		#limite pantalla()
+
         def moverAbajo(self):
                 self.rect.centery += self.velocidad
                 self.__movimiento()
                 self.direccion = 'S'
-                #limite patalla()
                 
         def __movimiento(self):
                 if self.sigueVivo():
@@ -77,30 +94,32 @@ class Personajes(pygame.sprite.Sprite):
                         return False
                 
         def aumentarVida(self,tiempo):
-                aumento=Curacion(self.rect.centerx,self.rect.centery,self.vida)
-
+                aumento = Curacion(self.rect.centerx,self.rect.centery,self.vida)
                 if tiempo >= self.ultimaCuracion + aumento.tiempoReutilizacion:
-                        self.vida+=aumento.cantAumento
-                        if self.vida>= self.maxVida:
-                                self.vida=self.maxVida
-                                self.listaCuracion.append(aumento)        
-                                self.ultimaCuracion=tiempo
-                                self.cooldownCuracion=aumento.tiempoReutilizacion
+                        self.listaCuracion.append(aumento)
                                 
-                """el if lo quise hacer en el metodo aumentar de la clase Curacion pero no me salio"""
         def recibirDanio(self, danio):
                 self.vida -= danio
                 
-        def disparar(self, tiempo):
-                disparo = Disparo(self.rect.centerx, self.rect.centery, self.direccion)
-                
+        def disparar(self, tiempo): 
+                disparo = Disparo(self.rect.centerx, self.rect.centery, self.direccion,10,6)
                 
                 if tiempo >= self.ultimoDisparo + disparo.tiempoReutilizacion:
                         self.listaDisparo.append(disparo)
                         self.ultimoDisparo = tiempo
+                        
+        def disparoLaser(self, tiempo):
+                 disparo=Disparo(self.rect.centerx,self.rect.centery,self.direccion,2,6)
+                 
+                 if tiempo >= self.ultimoDisparoLaser + disparo.tiempoReutilizacionLaser:
+                        for d in range (5,15):
+                                
+                                otrodisparo=Disparo(self.rect.centerx,self.rect.centery,self.direccion,2,d+1)
+                                self.listaDisparo.append(otrodisparo)
+                        self.listaDisparo.append(disparo)
+                        self.ultimoDisparoLaser = tiempo
+                        self.cooldownLaser=disparo.tiempoReutilizacionLaser
         
-                
-
 
                 
 class Curacion(pygame.sprite.Sprite):
@@ -108,39 +127,55 @@ class Curacion(pygame.sprite.Sprite):
 
         def __init__(self, posX, posY, vida):
                 pygame.sprite.Sprite.__init__(self)
-                self.imagenCuracion = pygame.image.load("Imagenes\Enemigo1.png")
+                self.imagenCuracion = pygame.image.load("Imagenes\Curacion.png")
                 self.rect = self.imagenCuracion.get_rect()
                 self.rect.centerx = posX
                 self.rect.centery = posY
                 self.cantAumento = 10
-                self.auxiliar=0
+                self.cantAumentoVelocidad = 5
+                self.duracionVelocidad = 3
+                self.usado = False
                 self.tiempoReutilizacion=5
               
-        """def aumentar(self):"""
+        def aumentar(self, jugador, tiempo):
+                jugador.vida+=self.cantAumento
+                jugador.velocidad += self.cantAumentoVelocidad
+                jugador.ultimaCuracion=tiempo
+                jugador.cooldownCuracion=self.tiempoReutilizacion
+                if jugador.vida>= jugador.maxVida:
+                        jugador.vida=jugador.maxVida
+                
         
         def dibujar(self, superficie):
                 superficie.blit(self.imagenCuracion,self.rect)
 
+        def reducirVelocidad(self, jugador):
+                jugador.velocidad -= self.cantAumentoVelocidad
+                
+        def trayectoria(self, jugador):
+                self.rect.centerx = jugador.rect.centerx
+                self.rect.centery = jugador.rect.centery
                      
                      
                      
-                     
+
 
         
                 
 class Disparo(pygame.sprite.Sprite):
         """Clase del disparo"""
         
-        def __init__(self, posX, posY, direccion):
+        def __init__(self, posX, posY, direccion,danio,velocidad):
                 pygame.sprite.Sprite.__init__(self)
-                self.imagenDisparo = pygame.image.load("Imagenes\Disparo.png")
+                self.imagenDisparo = pygame.image.load("Imagenes/Disparo.png")
                 self.rect = self.imagenDisparo.get_rect()
                 self.rect.centerx = posX
                 self.rect.centery = posY
-                self.danio = 10
-                self.velocidad = 6
+                self.danio = danio
+                self.velocidad = velocidad
                 self.direccion = direccion
                 self.tiempoReutilizacion = 1
+                self.tiempoReutilizacionLaser = 5
                 
         def trayectoria(self):
                 if self.direccion == 'N':
@@ -183,7 +218,7 @@ class Enemigo(pygame.sprite.Sprite):
                 superficie.blit(texto_vida, (self.rect.left +5, self.rect.bottom ))
 
         def disparar(self, tiempo):
-                disparo = Disparo(self.rect.centerx, self.rect.centery, self.direccion)
+                disparo = Disparo(self.rect.centerx, self.rect.centery, self.direccion,10,5)
                 if tiempo > self.ultimoDisparo + disparo.tiempoReutilizacion:
                         self.listaDisparo.append(disparo)
                         self.ultimoDisparo = tiempo
@@ -198,23 +233,22 @@ class Enemigo(pygame.sprite.Sprite):
                 self.rect.centerx -= self.velocidad
                 self.__movimiento()
                 self.direccion = 'O'
-                #limite de pantalla()
 
         def moverDerecha(self):
                 self.rect.centerx += self.velocidad
                 self.__movimiento()
                 self.direccion = 'E'
-                #limite de pantalla()
+
         def moverArriba(self):
                 self.rect.centery -= self.velocidad
                 self.__movimiento()
                 self.direccion = 'N'
-		#limite pantalla()
+
         def moverAbajo(self):
                 self.rect.centery += self.velocidad
                 self.__movimiento()
                 self.direccion = 'S'
-                #limite patalla()
+
                 
         def __movimiento(self):
                 if self.sigueVivo():
@@ -247,7 +281,12 @@ class Enemigo(pygame.sprite.Sprite):
                 return ((enemigo.rect.left < jugador.rect.centerx and enemigo.rect.right > jugador.rect.centerx and (self.direccion == 'S' or self.direccion == 'N')) or
                         (enemigo.rect.top < jugador.rect.centery and enemigo.rect.bottom > jugador.rect.centery and (self.direccion == 'E' or self.direccion == 'O')))
         
-                        
+def crearListaEnemigos(listaEnemigos, cantidad):
+        for i in range (0,cantidad):
+                posx = randint(100, ancho-100)
+                posy = randint(100, alto-100)
+                enemigo = Enemigo(posx, posy)
+                listaEnemigos.append(enemigo)
         
 """""""""""""""""""""""""""MAIN"""""""""""""""""""""""""""""""""        
 pygame.init()
@@ -259,59 +298,121 @@ imagenFondo = pygame.image.load("Imagenes\Fondo.jpg")
 ventana.blit(imagenFondo, (0, 0))
 
 jugador = Personajes()
-enemigo = Enemigo(ancho/2, 1)
+##enemigo = Enemigo(ancho/2, 1)
 tiempo = 1
+
+derechaApretada = False
+izquierdaApretada = False
+arribaApretada = False
+abajoApretada = False
+disparoApretado = False
+disparoLaserApretado=False
+
+listaEnemigos = []
+crearListaEnemigos(listaEnemigos, 3)
 
 while jugador.sigueVivo():
         tiempo_milesimas = pygame.time.get_ticks()/1000
         if tiempo == tiempo_milesimas:
-                print tiempo
+##                print tiempo
                 tiempo += 1
                 
                 if jugador.cooldownCuracion>0:    
-                        print jugador.cooldownCuracion
+##                      print jugador.cooldownCuracion
                         jugador.cooldownCuracion-=1
+##
+                if jugador.cooldownLaser>0:
+                        print jugador.cooldownLaser
+                        jugador.cooldownLaser-=1
                         
-        """if not jugador.sigueVivo():
-                Fuente = pygame.font.SysFont("Arial", 20)
-                texto_fin = Fuente.render("Perdiste" , 0, (0, 0, 0))
-                ventana.blit(texto_fin, (ancho/2, alto/2))"""
         for evento in pygame.event.get():
                 if evento.type == QUIT: #agregar la salida al menu y guardado de score
                         pygame.quit()
                         sys.exit()
                 if evento.type == pygame.KEYDOWN:
                         if evento.key == K_LEFT:
-                                jugador.moverIzquierda()
+                                izquierdaApretada = True
                         elif evento.key == K_RIGHT:
-                                jugador.moverDerecha()
+                                derechaApretada = True
                         elif evento.key == K_UP:
-                                jugador.moverArriba()
+                                arribaApretada = True
                         elif evento.key == K_DOWN:
-                                jugador.moverAbajo()
-                        elif evento.key == K_SPACE:
-                                jugador.disparar(tiempo)
-                if evento.type == pygame.KEYUP:
-                        if evento.key == K_1:
+                                abajoApretada = True
+                        elif evento.key == K_1:
                                 jugador.aumentarVida(tiempo)
-
+                        if evento.key == K_2:
+                                disparoLaserApretado=True
+                        elif evento.key ==K_SPACE:
+                                disparoApretado=True
+                                
+                                
+                                
+     
+                if evento.type == pygame.KEYUP:
+                        if evento.key == K_LEFT:
+                                izquierdaApretada = False
+                        elif evento.key == K_RIGHT:
+                                derechaApretada = False
+                        elif evento.key == K_UP:
+                                arribaApretada = False
+                        elif evento.key == K_DOWN:
+                                abajoApretada = False     
+                        elif evento.key == K_2 :
+                                disparoLaserApretado=False
+                        elif evento.key ==K_SPACE:
+                                disparoApretado=False
+                        
+                      
+        if disparoLaserApretado:      
+                jugador.disparoLaser(tiempo)                  
+        elif disparoApretado:
+                jugador.disparar(tiempo)
+                        
+        jugador.mover(izquierdaApretada, derechaApretada, arribaApretada, abajoApretada)
         ventana.blit(imagenFondo, (0, 0))
         jugador.dibujar(ventana)
         
-        
+        """
         if enemigo.sigueVivo():
                 enemigo.mover(jugador)
                 enemigo.dibujar(ventana)
                 if enemigo.enRango(jugador):
                         enemigo.disparar(tiempo)
-
+        """
+        if len(listaEnemigos) > 0:
+                for enemigo in listaEnemigos:
+                        enemigo.dibujar(ventana)
+                        enemigo.mover(jugador)
+                        if not enemigo.sigueVivo():
+                                listaEnemigos.remove(enemigo)
+                        elif enemigo.enRango(jugador):
+                                enemigo.disparar(tiempo)
+                        if len(enemigo.listaDisparo) > 0:
+                                for y in enemigo.listaDisparo:
+                                        y.dibujar(ventana)
+                                        y.trayectoria()
+                                        if y.rect.colliderect(jugador.rect):
+                                                jugador.recibirDanio(y.danio)
+                                                enemigo.listaDisparo.remove(y)
+                                        if y.rect.top < -20:
+                                                enemigo.listaDisparo.remove(y)
+                                        elif y.rect.bottom > alto+20:
+                                                enemigo.listaDisparo.remove(y)
+                                        elif y.rect.left < -20:
+                                                enemigo.listaDisparo.remove(y)
+                                        elif y.rect.right > ancho+20:
+                                                enemigo.listaDisparo.remove(y)
+        
         if len(jugador.listaDisparo) > 0: #aca recorro la lista de disparos pendientes del jugador
                 for x in jugador.listaDisparo:
                         x.dibujar(ventana)
                         x.trayectoria()
-                        if x.rect.colliderect(enemigo.rect):
-                                enemigo.recibirDanio(x.danio)
-                                jugador.listaDisparo.remove(x)
+                        if len(listaEnemigos) > 0:
+                                
+                                for enemigo in listaEnemigos:
+                                        if x.rect.colliderect(enemigo.rect):
+                                                enemigo.recibirDanio(x.danio)
+                                                jugador.listaDisparo.remove(x)
                         if x.rect.top < -20:
                                 jugador.listaDisparo.remove(x)
                         elif x.rect.bottom > alto+20:
@@ -321,22 +422,19 @@ while jugador.sigueVivo():
                         elif x.rect.right > ancho+20:
                                 jugador.listaDisparo.remove(x)
 
-        if len(enemigo.listaDisparo) > 0:
-                for y in enemigo.listaDisparo:
-                        y.dibujar(ventana)
-                        y.trayectoria()
-                        if y.rect.colliderect(jugador.rect):
-                                jugador.recibirDanio(y.danio)
-                                enemigo.listaDisparo.remove(y)
-                        if y.rect.top < -20:
-                                enemigo.listaDisparo.remove(y)
-                        elif y.rect.bottom > alto+20:
-                                enemigo.listaDisparo.remove(y)
-                        elif y.rect.left < -20:
-                                enemigo.listaDisparo.remove(y)
-                        elif y.rect.right > ancho+20:
-                                enemigo.listaDisparo.remove(y)
                                 
+        if len(jugador.listaCuracion) > 0:
+                for z in jugador.listaCuracion:
+                        z.dibujar(ventana)
+                        z.trayectoria(jugador)
+                        if not z.usado:
+                                z.aumentar(jugador, tiempo)
+                                z.usado = True
+                        elif jugador.ultimaCuracion + z.duracionVelocidad <= tiempo:
+                                z.reducirVelocidad(jugador)
+                                jugador.listaCuracion.remove(z)
+
+        
         pygame.display.update()
         
 pygame.quit()
